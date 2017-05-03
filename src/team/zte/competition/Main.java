7366353e7aa03f19dde9ea5ec0d9660aa4572c70
coderooookie <shubinsjtu@qq.com>
2017-05-03 19:04:21 +0800

    加入迪杰斯特拉算法

--- src/team/zte/competition/Main.java
+++ src/team/zte/competition/Main.java
@@ -4,7 +4,9 @@ package team.zte.competition;
 import java.io.FileInputStream;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
+
 
 
 public class Main {
@@ -14,6 +16,10 @@ public class Main {
 	static int[][] allGraph;//拆点后的邻接矩阵
 	static ArrayList<Integer> nodeCstrnt = new ArrayList<Integer>();//必须经过的点
 	static ArrayList<Integer> edgeCstrnt = new ArrayList<Integer>();//必须经过的点
+	static boolean[] visited;//判断是否访问过
+	static int[] pre;//保留上一个节点
+	static List<Integer> path;
+	
 	
 	public static void main(String[] args){
 		try {
@@ -24,7 +30,44 @@ public class Main {
 		checkMatrix(graph);
 		graphTrans(nodeCstrntNum+edgeCstrntNum);
 		checkMatrix(allGraph);
+		dijkstra();
+		checkMatrix(allGraph);
+		genPath();
+		showPath();
+		
 	}
+	//暴力迪杰斯特拉
+	public static void dijkstra(){
+		visited = new boolean[allGraph.length];
+		pre = new int[allGraph.length];
+		Arrays.fill(visited, false);
+		visited[0] = true;
+		pre[0] = -1;
+		int visiting = 0, tmpNode = 0, tmpCost;
+		while (visiting != allGraph.length - 1){
+			//找寻下一个节点
+			tmpCost = Integer.MAX_VALUE;
+			for (int i = 0; i < allGraph.length; i++){
+				if (!visited[i] && allGraph[0][i] < tmpCost){
+					tmpCost = allGraph[0][i];
+					tmpNode = i;
+				}
+			}
+			
+			//更新花费表
+			for (int i = 0; i < allGraph.length; i++){
+				if ((allGraph[0][tmpNode] < Integer.MAX_VALUE && allGraph[tmpNode][i] < Integer.MAX_VALUE) && ((allGraph[0][tmpNode] + allGraph[tmpNode][i]) < allGraph[0][i])){
+					allGraph[0][i] = (allGraph[0][tmpNode] + allGraph[tmpNode][i]);
+					pre[i] = tmpNode;
+				}
+			}
+			//更新访问情况
+			visited[tmpNode] = true;
+			visiting = tmpNode;
+			System.out.println("已遍历点"+visiting);
+		}
+	}
+	
 	
 	//该函数用于生成拆点后的邻接矩阵
 	public static void graphTrans(int cstrntNum){
@@ -49,9 +92,8 @@ public class Main {
 				}
 			}
 		}
-		//加入必经过边约束
-		
-		
+		//todo : 加入必经过边约束
+
 	}
 	
 	//读取文件中的数据
@@ -64,6 +106,9 @@ public class Main {
 			graph = new int[nodeNum][nodeNum];
 			for (int i = 0; i < nodeNum; i++){
 				Arrays.fill(graph[i], Integer.MAX_VALUE);
+			}
+			for (int i = 0; i < nodeNum; i++){
+				graph[i][i] = 0;
 			}
 			for (int i = 0; i < edgeNum; i++){
 				int begin = sc.nextInt(), end = sc.nextInt(), len = sc.nextInt();
@@ -83,7 +128,25 @@ public class Main {
 		sc.close();
 		fis.close();
 	}
-	
+	//生成路径
+	public static void genPath(){
+		int node = allGraph.length - 1;
+		path = new ArrayList<Integer>();
+		while (pre[node] != -1){
+			if ((node-pre[node])%nodeNum != 0){
+				path.add(node%nodeNum);
+			}
+			node = pre[node];
+		}
+		path.add(node);
+	}
+	//输出路径
+	public static void showPath(){
+		System.out.print("路径为 ：\t");
+		for (int i = path.size(); i > 0; i--){
+			System.out.printf("%d\t",path.get(i - 1));
+		}
+	}
 	
 	
 	public static void readString(String file) throws Exception{
@@ -101,6 +164,7 @@ public class Main {
 	public static void checkMatrix(int[][] graph){
 		System.out.println("矩阵为：");
 		for (int i = 0; i < graph.length; i++){
+			System.out.printf("%d:\t",i);
 			for (int j = 0; j < graph[0].length; j++){
 				System.out.printf("%d\t",graph[i][j]);
 			}
